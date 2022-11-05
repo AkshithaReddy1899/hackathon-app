077e26b038c79241476cf8cef549cbc7c9038229
Akshitha Reddy <akshithareddy1899@gmail.com>
2022-11-05 20:02:58 +0530

    Routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
 import Nav from './components/Nav';
 import Main from './components/Main';
+import Form from './components/Form';
 
 function App() {
   return (
-    <div className="">
+    <>
       <Nav />
-      <Main />
-    </div>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/form" element={<Form />} />
+      </Routes>
+    </>
   );
 }
 
